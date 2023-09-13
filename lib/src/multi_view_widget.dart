4b27129f6bb0f34f3228dacd3a17af5110a78906
abdelrahman-abied <abied_2011@yahoo.com>
2023-09-13 20:51:11 +0300

    add multiview

--- lib/src/multi_view_widget.dart
+++ lib/src/multi_view_widget.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
-class MutliView extends StatelessWidget {
+class MultiViewWidget extends RepaintBoundary {
   final Widget child;
-  const MutliView({Key? key, required this.child}) : super(key: key);
+  const MultiViewWidget({Key? key, required this.child}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
